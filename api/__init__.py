cbf81399d270eee300a84b538ac8c880907a1e38
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-08-24 09:33:37 +0100

    Add Flask-Migrate for database migrations and update SQLAlchemy configuration

--- api/__init__.py
+++ api/__init__.py
@@ -6,15 +6,30 @@ from .config.config import config_dict
 from .utils import db
 from .models.orders import Order
 from .models.users import User
+from flask_migrate import Migrate
+
 
 def create_app(config=config_dict['dev']):
   app=Flask(__name__)
 
+
   app.config.from_object(config)
+
+  db.init_app(app)
+
+  migrate=Migrate(app,db)
 
   api=Api(app)
 
   api.add_namespace(order_namespace)
   api.add_namespace(auth_namespace)
+
+  @app.shell_context_processor
+  def make_shell_context():
+    return {
+      'db':db,
+      'Order':Order,
+      'User':User
+    }
 
   return app
--- api/models/orders.py
+++ api/models/orders.py
@@ -2,14 +2,14 @@ from ..utils import db
 from enum import Enum
 from datetime import datetime
 
-class Sizes:
+class Sizes(Enum):
   SMALL ='small'
   MEDIUM ='medium'
   LARGE ='large'
   EXTRA_LARGE ='extra_large'
 
 
-class OrderStatus:
+class OrderStatus(Enum):
   PENDING='pending'
   IN_TRANSIT ='in_transit'
   DELIVERED ='delivered'
@@ -18,10 +18,10 @@ class OrderStatus:
 class Order(db.Model):
   __tablename__ = 'orders'
   id=db.Column(db.Integer(), primary_key=True)
-  size=db.column(db.Enum(Sizes),default=Sizes.SMALL)
-  order_status=db.column(db.Enum(OrderStatus),default=OrderStatus.PENDING)
-  flavor=db.column(db.String(), nullable=False)
-  date_created=db.column(db.DateTime(),default=datetime.utc)
+  size=db.Column(db.Enum(Sizes), default=Sizes.SMALL)
+  order_status=db.Column(db.Enum(OrderStatus),default=OrderStatus.PENDING)
+  flavor=db.Column(db.String(), nullable=False)
+  date_created=db.Column(db.DateTime(),default=datetime.utcnow)
   user=db.Column(db.Integer(),db.ForeignKey('users.id'), nullable=False)
 
   def __rep__(self):
